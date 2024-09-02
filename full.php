7147bb052869bb16f48ccb4f24a679b1af4df6e1
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-02 13:26:58 +0700

    Update full.php

--- full.php
+++ full.php
@@ -1,1 +1,59 @@
 
+<?php
+
+// Lib/Person.php
+
+class Person {
+
+public function __construct(
+
+private string $name
+
+) {
+
+}
+
+public function speak() {
+
+echo "Hi, my name is $this->name";
+
+}
+
+}
+
+<?php
+
+// functions.php
+
+function dd($value) {
+
+echo '<pre>'; // respect line spaces in the $value
+
+var_dump($value);
+
+echo '</pre>';
+
+die();
+
+}
+
+function basePath($path) {
+
+return BASE_PATH . $path;
+
+}
+
+<?php
+
+// public/index.php
+
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . 'functions.php';
+
+require basePath('Lib/Person.php');
+
+$person = new Person("Danny");
+
+$person->speak();
+
